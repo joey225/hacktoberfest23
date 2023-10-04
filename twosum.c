8045805a0791871c0fededbfcfcd2b925f9fb5d1
joey225 <146970323+joey225@users.noreply.github.com>
2023-10-04 20:48:51 +0530

    twosum.c

---
+++ twosum.c
@@ -0,0 +1,7 @@
+echo "# hacktoberfest23" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/joey225/hacktoberfest23.git
+git push -u origin main
